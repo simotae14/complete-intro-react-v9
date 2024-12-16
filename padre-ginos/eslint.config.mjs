4c4303b10800a690f5f6cffbab6d8d145d18998c
Simone Taeggi <startae14@gmail.com>
2024-12-16 21:42:16 +0100

    use TanStack Query to retrieve data

--- padre-ginos/eslint.config.mjs
+++ padre-ginos/eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,6 +17,7 @@ export default [
   },
   // fix the error not importing react
   reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
