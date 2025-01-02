952d0127910d50b8f9341f41b780bd8b818a5954
Simone Taeggi <startae14@gmail.com>
2025-01-02 22:52:58 +0100

    TDD on a Component

--- padre-ginos/src/__tests__/Pizza.test.jsx
+++ padre-ginos/src/__tests__/Pizza.test.jsx
@@ -1,15 +1,26 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
 import Pizza from "../Pizza";
 
-test('alt text renders on Pizza image', async () => {
+afterEach(cleanup);
+
+test("alt text renders on Pizza image", async () => {
   const name = "My Favorite Pizza";
   const src = "https://picsum.photos/200";
   const screen = render(
-    <Pizza name={name} description="super cool pizza" image={src} />
+    <Pizza name={name} description="super cool pizza" image={src} />,
   );
 
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name="something else" description="super cool pizza" />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
 });
