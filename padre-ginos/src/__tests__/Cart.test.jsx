a5f28e59fc63bdf17036ef1877d186d3f44fddcd
Simone Taeggi <startae14@gmail.com>
2025-01-22 17:21:02 +0100

    Snapshot testing usefull case test Response API objects

--- padre-ginos/src/__tests__/Cart.test.jsx
+++ padre-ginos/src/__tests__/Cart.test.jsx
@@ -2,7 +2,20 @@ import { expect, test } from "vitest";
 import { render } from "@testing-library/react";
 import Cart from "../Cart";
 
-test("snapsto with nothing in cart", () => {
+test("snapshot with nothing in cart", () => {
   const { asFragment } = render(<Cart cart={[]} />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+// usefull use case for snapshot testing is to test an object with a lot of properties
+// coming from an API
+test("lol", () => {
+  expect({
+    id: "calabrese",
+    name: "The Calabrese Pizza",
+    category: "Supreme",
+    description: "lol pizza from Calabria",
+    image: "/public/pizzas/calabrese.webp",
+    size: { S: 12.25, M: 16.25, L: 20.25 },
+  }).toMatchSnapshot();
 });
