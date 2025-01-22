b4c621e880fb61ec10ba1aef633a8fee5df27e69
Simone Taeggi <startae14@gmail.com>
2025-01-22 17:23:46 +0100

    Snapshot Testing Inline

--- padre-ginos/src/__tests__/Cart.test.jsx
+++ padre-ginos/src/__tests__/Cart.test.jsx
@@ -4,18 +4,22 @@ import Cart from "../Cart";
 
 test("snapshot with nothing in cart", () => {
   const { asFragment } = render(<Cart cart={[]} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-// usefull use case for snapshot testing is to test an object with a lot of properties
-// coming from an API
-test("lol", () => {
-  expect({
-    id: "calabrese",
-    name: "The Calabrese Pizza",
-    category: "Supreme",
-    description: "lol pizza from Calabria",
-    image: "/public/pizzas/calabrese.webp",
-    size: { S: 12.25, M: 16.25, L: 20.25 },
-  }).toMatchSnapshot();
+  expect(asFragment()).toMatchInlineSnapshot(`
+    <DocumentFragment>
+      <div
+        class="cart"
+      >
+        <h2>
+          Cart
+        </h2>
+        <ul />
+        <p>
+          Total: $0.00
+        </p>
+        <button>
+          Checkout
+        </button>
+      </div>
+    </DocumentFragment>
+  `);
 });
