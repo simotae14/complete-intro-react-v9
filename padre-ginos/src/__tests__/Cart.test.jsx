03acd1ce752993617d4ae944347ae2325025fae9
Simone Taeggi <startae14@gmail.com>
2025-01-22 17:24:21 +0100

    remove inline snapshot

--- padre-ginos/src/__tests__/Cart.test.jsx
+++ padre-ginos/src/__tests__/Cart.test.jsx
@@ -4,22 +4,5 @@ import Cart from "../Cart";
 
 test("snapshot with nothing in cart", () => {
   const { asFragment } = render(<Cart cart={[]} />);
-  expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
-      <div
-        class="cart"
-      >
-        <h2>
-          Cart
-        </h2>
-        <ul />
-        <p>
-          Total: $0.00
-        </p>
-        <button>
-          Checkout
-        </button>
-      </div>
-    </DocumentFragment>
-  `);
+  expect(asFragment()).toMatchSnapshot();
 });
