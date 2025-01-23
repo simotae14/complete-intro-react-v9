ca07619378febf3cbc8b6e692e235c57447c0527
Simone Taeggi <startae14@gmail.com>
2025-01-23 01:16:18 +0100

    Code Coverage with Istanbul

--- padre-ginos/vitest.workspace.js
+++ padre-ginos/vitest.workspace.js
@@ -7,6 +7,10 @@ export default defineWorkspace([
       include: ["**/*.node.test.{js,jsx}"],
       name: "happy-dom",
       environment: "happy-dom",
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
     },
   },
   {
@@ -15,10 +19,13 @@ export default defineWorkspace([
       setupFiles: ["vitest-browser-react"],
       include: ["**/*.browser.test.{js,jsx}"],
       name: "browser",
+      coverage: {
+        reporter: ["text", "json", "html"],
+      },
       browser: {
         provider: "playwright",
         enabled: true,
-        name: "firefox",
+        name: "chromium",
       },
     },
   },
